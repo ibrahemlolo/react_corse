1d18c005a3449473e41509c21d3768296de483cb
ibrahemlolo <ibrahemlolo.sy@gmail.com>
2024-10-02 21:01:06 +0300

    useState_example1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+     const [Ibra, setIbra] = React.useState(true)
+     function Taggle() {
+          setIbra(prev => !prev)
+          console.log(String(Ibra))     
+     }
+     return (
+          <div style={{ fontSize:"20px", textAlign:"center" }}>
+               <h1 onClick={Taggle}>{ Ibra ? "Yes":"No" }</h1>
+          </div>
+     )
 }
-
-export default App;

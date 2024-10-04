0aaeff02cbba4f3c9cd6eec405d156862e8f4282
ibrahemlolo <ibrahemlolo.sy@gmail.com>
2024-10-05 01:13:02 +0300

    last train on useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import Name from "./components/Name"
 
 export default function App() {
-     useEffect(() =>
-     {
+
+     const [data, setData] = useState([]);
+
+     const datashow = data.map((item,index) => <Name img={item[0]} name={item[1]} key={index} />);
+
+     useEffect(() => {
           fetch('https://api.imgflip.com/get_memes')
                .then((res) => res.json())
-               .then((data) => console.log(data.data.memes.map((item)=> item.name)))}
-     )
-     return (
-          <div >
-               
-          </div>
+               .then((dat) => setData(dat.data.memes.map((item) => [item.url,item.name])))
+     },[]);
+     
 
-     )
+     return (
+          <div>
+               {datashow}
+          </div>)
+
+
 }

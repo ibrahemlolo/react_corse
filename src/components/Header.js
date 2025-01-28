b8c9320c59f29a68bce1121e5f0f9c3e6ea06f03
ibrahemlolo <ibrahemlolo.sy@gmail.com>
2025-01-28 15:32:43 +0300

    ibra97

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 export default function Header() {
      return (
-          <div>
-               <head></head>
-          </div>
+               <nav>
+                    <h6>Home</h6>
+                    <h6>About</h6>
+                    <div style={{ textAlign: "center"}} className="regester">
+                         Regester
+                    </div>
+               </nav>
      )
 }
